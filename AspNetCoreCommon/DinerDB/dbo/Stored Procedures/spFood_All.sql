7fd08e67003d31080c3437beb2aaa2b5d38c6dd4
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2020-07-16 15:44:52 -0700

    Finished setting up store procedures

---
+++ AspNetCoreCommon/DinerDB/dbo/Stored Procedures/spFood_All.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spFood_All]
+AS
+begin
+	set nocount on;
+	select * from dbo.Food;
+	
+end
---
+++ AspNetCoreCommon/DinerDB/dbo/Stored Procedures/spOrders_GetById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spOrders_GetById]
+	@Id int
+AS
+begin
+	select nocount on;
+
+	select * from dbo.[order]
+	where Id = @Id;
+
+end
